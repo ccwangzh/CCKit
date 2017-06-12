8c1a3827b240b45f14a1654e5c5cad3a52992a15
kevin01.wang <kevin01.wang@vipshop.com>
2017-06-12 15:35:24 +0800

    设置加载页

---
+++ CCKitSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCKitSDK
+//
+//  Created by can on 17/6/12.
+//  Copyright © 2017年 womob.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCKitSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCKitSDK
+//
+//  Created by can on 17/6/12.
+//  Copyright © 2017年 womob.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
