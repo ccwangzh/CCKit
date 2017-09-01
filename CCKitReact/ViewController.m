bba7ad5e606febfb1535d136e9ce17c5a5bb684e
kevin01.wang <kevin01.wang@vipshop.com>
2017-09-01 19:17:33 +0800

    更改react-native模块

--- CCKitReact/ViewController.m
+++ CCKitReact/ViewController.m
@@ -26,7 +26,7 @@
     jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
     
     RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                        moduleName:@"CCKit"
+                                                        moduleName:@"RootView"
                                                  initialProperties:nil
                                                      launchOptions:nil];
     rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
--- CCKitReact/index.ios.js
+++ CCKitReact/index.ios.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-export default class CCKit extends Component {
+export default class RootView extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('CCKit', () => CCKit);
+AppRegistry.registerComponent('RootView', () => RootView);
