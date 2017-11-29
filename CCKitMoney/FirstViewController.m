f7ca4a710557db2af0215e8d0d50aecc4f4f5a06
kevin01.wang <kevin01.wang@vipshop.com>
2017-11-29 18:17:47 +0800

    增加CCKitMoney&News

---
+++ CCKitMoney/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  CCKitMoney
+//
+//  Created by can on 2017/11/29.
+//  Copyright © 2017年 womob.com. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ CCKitMoney/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  CCKitMoney
+//
+//  Created by can on 2017/11/29.
+//  Copyright © 2017年 womob.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
